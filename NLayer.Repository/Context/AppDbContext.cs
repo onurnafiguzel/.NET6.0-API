3da32ace4e3a77bfafadb038ce0b1fe2f8123a08
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-01-16 16:34:01 +0300

    AppDbContext

---
+++ NLayer.Repository/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) // Veritabanı yolu startup'tan verilecek
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
