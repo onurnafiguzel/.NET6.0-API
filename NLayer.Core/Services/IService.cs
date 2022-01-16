4e7607293839ca72898b400f37a04f50fc5188b0
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-01-16 21:49:48 +0300

    Service

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -9,12 +9,12 @@ namespace NLayer.Core.Services
 {
     public interface IService<T> where T : class
     {
-        Task<T> GetByIdAsync<T>(int id);
+        Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-        Task AddAsnyc(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<T> AddAsnyc(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsnyc(T entity);
         Task RemoveAsnyc(T entity);
         Task RemoveRangeAsync(IEnumerable<T> entities);
