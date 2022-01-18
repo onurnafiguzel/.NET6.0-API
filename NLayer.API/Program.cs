13d2dc08cdc6ea4e70dd3e99d1e2f8ebac5cf1eb
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-01-18 23:05:32 +0300

    CachingService

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -30,6 +30,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMemoryCache();
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
 builder.Services.AddAutoMapper(typeof(MapProfile));
 builder.Services.AddDbContext<AppDbContext>(x =>
---
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Caching.Memory;
+using NLayer.Core.Dtos;
+using NLayer.Core.Entities;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Caching
+{
+    internal class ProductServiceWithCaching : IProductService
+    {
+        private const string CacheProductKey = "productsCache";
+        private readonly IMapper _mapper;
+        private readonly MemoryCache _memoryCache;
+        private readonly IProductRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductServiceWithCaching(IUnitOfWork unitOfWork, IProductRepository repository, MemoryCache memoryCache, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+            _memoryCache = memoryCache;
+            _mapper = mapper;
+
+            if (!memoryCache.TryGetValue(CacheProductKey, out _))
+            {
+                _memoryCache.Set(CacheProductKey, _repository.GetAll().ToList());
+            }
+        }
+
+        public Task<Product> AddAsnyc(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsnyc(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsnyc(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> Where(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
