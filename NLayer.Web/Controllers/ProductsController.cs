0744eee6703d7047cdaabb35f6015e26ac06e3b0
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-01-21 14:23:05 +0300

    ProductController implementasyonu yapıldı ve Save methoduna başlandı.

---
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core.Dtos;
+using NLayer.Core.Entities;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductService productService, ICategoryService categoryService, IMapper mapper)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productService.GetProductsWithCategory());
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsnyc(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
+        }
+    }
+}
